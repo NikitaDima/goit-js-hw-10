25148b771514fb7ca1a6049bc132ffb7ec2fbfc9
NikitaDima <onekill13@gmail.com>
2023-03-11 06:52:00 +0200

    go

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const ref = {
+  searchInput: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+};
+
+ref.searchInput.addEventListener('input', () => {
+  console.log(ref.searchInput.value);
+});
