451d07e0f175c8e0c70733874a468cf0e38efe23
NikitaDima <113444983+NikitaDima@users.noreply.github.com>
2023-03-12 05:35:50 +0000

    Deploying to gh-pages from @ NikitaDima/goit-js-hw-10@f954fd36c70bd8390346d86587f1bb1f1baf686d 🚀

--- index.3a477416.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={searchInput:document.querySelector("#search-box"),countryList:document.querySelector(".country-list")};e.searchInput.addEventListener("input",(function(){console.log(e.searchInput.value)}))}();
-//# sourceMappingURL=index.3a477416.js.map
